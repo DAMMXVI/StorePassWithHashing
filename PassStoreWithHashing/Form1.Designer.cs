8343e679155b787353d79c884c103ad0f4d52b28
Alper <alpdnz@users.noreply.github.com>
2019-10-05 15:35:41 +0300

    Update

--- PassStoreWithHashing/Form1.Designer.cs
+++ PassStoreWithHashing/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblPassControl = new System.Windows.Forms.Label();
+            this.btnPassControl = new System.Windows.Forms.Button();
+            this.txtPassControl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(131, 25);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(164, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(131, 61);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(164, 20);
+            this.txtPass.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLogin.Location = new System.Drawing.Point(131, 97);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(164, 29);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(41, 32);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "ID";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(41, 68);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(53, 13);
+            this.lblPass.TabIndex = 4;
+            this.lblPass.Text = "Password";
+            // 
+            // lblPassControl
+            // 
+            this.lblPassControl.AutoSize = true;
+            this.lblPassControl.Location = new System.Drawing.Point(41, 154);
+            this.lblPassControl.Name = "lblPassControl";
+            this.lblPassControl.Size = new System.Drawing.Size(53, 13);
+            this.lblPassControl.TabIndex = 7;
+            this.lblPassControl.Text = "Password";
+            // 
+            // btnPassControl
+            // 
+            this.btnPassControl.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPassControl.Location = new System.Drawing.Point(131, 183);
+            this.btnPassControl.Name = "btnPassControl";
+            this.btnPassControl.Size = new System.Drawing.Size(164, 29);
+            this.btnPassControl.TabIndex = 6;
+            this.btnPassControl.Text = "Password Control";
+            this.btnPassControl.UseVisualStyleBackColor = true;
+            this.btnPassControl.Click += new System.EventHandler(this.btnPassControl_Click);
+            // 
+            // txtPassControl
+            // 
+            this.txtPassControl.Location = new System.Drawing.Point(131, 147);
+            this.txtPassControl.Name = "txtPassControl";
+            this.txtPassControl.Size = new System.Drawing.Size(164, 20);
+            this.txtPassControl.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(399, 250);
+            this.Controls.Add(this.lblPassControl);
+            this.Controls.Add(this.btnPassControl);
+            this.Controls.Add(this.txtPassControl);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblPassControl;
+        private System.Windows.Forms.Button btnPassControl;
+        private System.Windows.Forms.TextBox txtPassControl;
     }
 }
 
--- PassStoreWithHashing/Form1.cs
+++ PassStoreWithHashing/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PassStoreWithHashing
 {
@@ -15,6 +11,64 @@ namespace PassStoreWithHashing
         public Form1()
         {
             InitializeComponent();
+        }
+        //Define variables
+        string ID, pass, hashed_password, salt;
+        StringBuilder Sb = new StringBuilder();
+        char[] split = { ':' };
+        string[] splitted;
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            ID = txtID.Text;
+            pass = txtPass.Text;
+            hashed_password = hash_password(pass);
+            MessageBox.Show(hashed_password);
+            btnLogin.Enabled = false;
+        }
+
+        private void btnPassControl_Click(object sender, EventArgs e)
+        {
+            pass = txtPassControl.Text;
+            if (check_password(hashed_password, pass))
+                MessageBox.Show("Congratulations Correct!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Opps Wrong!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        public string hash_password(string password)
+        {
+
+            using (var hash = SHA256.Create())
+            {
+                salt = Guid.NewGuid().ToString();
+                Encoding enc = Encoding.UTF8;
+
+                byte[] result = hash.ComputeHash(enc.GetBytes(password + salt));
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+                Sb.Append(":" + salt);
+            }
+            return Sb.ToString();
+        }
+        public bool check_password(string hashed_password, string user_password)
+        {
+            splitted = hashed_password.Split(split);
+            pass = splitted[0];
+            salt = splitted[1];
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+
+                byte[] result = hash.ComputeHash(enc.GetBytes(user_password + salt));
+                Sb.Clear();
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+                user_password = Sb.ToString();
+            }
+            return pass == user_password;
+
         }
     }
 }
